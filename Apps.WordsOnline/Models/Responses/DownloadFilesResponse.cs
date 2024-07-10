fe64395fbaf2e958ac19b854339bc80879fb5962
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-07-10 17:40:10 +0300

    Added languages to output

--- Apps.WordsOnline/Models/Responses/DownloadFilesResponse.cs
+++ Apps.WordsOnline/Models/Responses/DownloadFilesResponse.cs
@@ -1,8 +1,23 @@
-﻿using Blackbird.Applications.Sdk.Common.Files;
+﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Files;
 
 namespace Apps.WordsOnline.Models.Responses;
 
 public class DownloadFilesResponse
 {
-    public List<FileReference> Files { get; set; } = new();
+    public List<FileObject> Files { get; set; } = new();
+}
+
+public class FileObject
+{
+    public FileReference File { get; set; } = new();
+
+    [Display("Source language")]
+    public string SourceLanguage { get; set; } = string.Empty;
+    
+    [Display("Target language")]
+    public string TargetLanguage { get; set; } = string.Empty;
+    
+    [Display("File name")]
+    public string FileName { get; set; } = string.Empty;
 }
